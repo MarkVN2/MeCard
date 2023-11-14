fd7c3f72624c4f2ba32fa4237dfe64ecd6b34245
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-11-14 18:47:34 -0300

    [setup]: dependencies and base code
    setting up next.js, taildwindcss, motion-framer and post-css.

---
+++ app/components/card/index.tsx
@@ -0,0 +1,11 @@
+import {motion} from "framer-motion"
+import Image from 'next/image'
+import { Children, useState } from "react";
+
+const Card = ({children}) => {
+
+    return(
+        <h1>{children}</h1>
+    )
+    //Nothing here for now just a setup
+}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import './global.css'
+import Image from 'next/image'
+import { useState } from "react";
+
+
+const Home = () =>{
+   
+    return(
+        <h1>Hello World!</h1>
+    )
+  }
+  
+export default Home
